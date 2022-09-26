34e75ed7c7b127f31c1bf4c51e16cee43382e96c
fhvfn94 <fhvfn94@gmail.com>
2022-09-26 21:33:29 +0300

    add 1, 2 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 67;
+        short aShort = 987;
+        short bShort = 678;
+        short cShort = 965;
+        short dShort =  549;
+        short eShort = 569;
+        short fShort = -159;
+        int aInt = 27897;
+        long aLong = 136854775808;
+        double aDouble = 27.12;
+        double bDouble = 2.786;
+        boolean aBoolean = false;
+        System.out.println();
     }
 }
